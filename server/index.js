3a41bae20364daacf9181fd47bf031ab701d0ecf
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-11-09 16:51:59 +0100

    Comment why using commonJS and not ES6 Apply ESLInt rules

--- server/index.js
+++ server/index.js
@@ -1,33 +1,40 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-const db = require('./db')
-const movieRouter = require('./routes/movie-router')
-const userRouter = require('./routes/user-router')
-
-const app = express()
-const apiPort = 3000
-
 /**
- * Copied sample code
- * TODO: change and add comment 
- */ 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
-
-/**
- * TODO: Comment 
- * This function ...
- * @param {} ...
- * @returns {} ...
+ * * Why we dont use ES6 but CommonJS in server files
+ * * 'import' vs 'require'
+ * You can't selectively load only the pieces you need with 'require'
+ * but with 'imports', you can selectively load only the pieces you need.
+ * That can save memory.
+ * Loading is synchronous(step by step) for 'require'
+ * on the other hand 'import' can be asynchronous(without waiting for previous import)
+ * so it can perform a little better than 'require'.
+ * "Node.js fully supports ECMAScript modules as they are currently specified
+ * and provides limited interoperability
+ * between them and the existing module format, CommonJS."
+ * "Expect major changes in the implementation
+ * including interoperability support, specifier resolution, and default behavior."
+ * Therefore we stick with commonJS.
  */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const db = require('./db');
+const movieRouter = require('./routes/movie-router');
+const userRouter = require('./routes/user-router');
+
+// * Create express app
+const app = express();
+const apiPort = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.use('/api', movieRouter),
-app.use('/api', userRouter)
+app.use('/api', movieRouter), app.use('/api', userRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
