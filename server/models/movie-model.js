b2db6344e6aee9d9bb84d57efd7b804a31cd2e98
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-11-09 16:51:02 +0100

    Apply ESLint rules and prettify files

--- server/models/movie-model.js
+++ server/models/movie-model.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const Movie = new Schema(
-    {
-        name: { type: String, required: true },
-        time: { type: [String], required: true },
-        rating: { type: Number, required: true },
-    },
-    { timestamps: true },
-)
+  {
+    name: { type: String, required: true },
+    time: { type: [String], required: true },
+    rating: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('movies', Movie)
+module.exports = mongoose.model('movies', Movie);
