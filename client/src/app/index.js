146c20b67c32a5ea77753ec22c1e04ea97046abb
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-11-09 18:09:16 +0100

    Add comments Apply ESLint rules

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { NavBar } from '../components'
-import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages'
+// Import my components
+import { NavBar } from '../components';
+import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+// Import Bootstrap’s ready-to-use css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// * The app’s entry point
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Switch>
-                <Route path="/movies/list" exact component={MoviesList} />
-                <Route path="/movies/create" exact component={MoviesInsert} />
-                <Route
-                    path="/movies/update/:id"
-                    exact
-                    component={MoviesUpdate}
-                />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/movies/list" exact component={MoviesList} />
+        <Route path="/movies/create" exact component={MoviesInsert} />
+        <Route path="/movies/update/:id" exact component={MoviesUpdate} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
