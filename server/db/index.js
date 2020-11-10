bace2f1bc2a4b4bfa57258a1f646284e950c86fa
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-11-10 18:43:25 +0100

    Add comments

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,11 @@
+/**
+ * * Mongoose
+ * At the time of writing Mongoose is by far the most popular ODM,
+ * and is a reasonable choice if you're using MongoDB for your database.
+ */
 const mongoose = require('mongoose');
 
+// Set up a connection with the database
 mongoose
   .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((e) => {
