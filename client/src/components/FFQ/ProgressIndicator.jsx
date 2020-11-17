1d92e7da500db5b90ccc84b2d3e40466a58b8f5d
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-11-17 14:46:02 +0100

    Add progress indicator component to FFQParticipation

---
+++ client/src/components/FFQ/ProgressIndicator.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const ProgressIndicator = (props) => {
+  const { currentQuestionID, numberOfQuestions } = props;
+  const percentage = `${Math.trunc((currentQuestionID / numberOfQuestions) * 100)}%`;
+  return (
+    <div className="mt-2">
+      <div className="d-flex justify-content-center">
+        <span className="badge badge-info">
+          {currentQuestionID} of {numberOfQuestions} answered
+        </span>
+      </div>
+      <div className="progress mt-2">
+        <div
+          className="progress-bar bg-info"
+          role="progressbar"
+          style={{ width: percentage }}
+          aria-valuenow={percentage}
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          {percentage}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressIndicator;
