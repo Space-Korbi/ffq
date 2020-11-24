b811987964ffaa34752b610b620e625a9a3e3094
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-11-24 02:04:48 +0100

    Remove sample code and apply ESLint rules Clean up routes

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,31 +1,13 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import authenticationService from '../services';
+import { Role } from '../helpers';
 import PrivateRoute from '../components/PrivateRoute';
-import AuthenticationService from '../services/AuthenticationService';
-import Role from '../helpers/role';
-
-// Import my components
+import LoginPage from '../pages/LoginPage';
 import Dashboard from '../components/Dashboard';
-
-const users = [
-  {
-    id: 1,
-    username: 'admin',
-    password: 'admin',
-    firstName: 'Admin',
-    lastName: 'User',
-    role: Role.Admin
-  },
-  {
-    id: 3,
-    username: 'participant',
-    password: 'participant',
-    firstName: 'Participant',
-    lastName: 'User',
-    role: Role.Participant
-  }
-];
 
 /**
  * * Routing
@@ -33,19 +15,18 @@ const users = [
  * Order is important. Since the router will exit as soon as it finds a match,
  * always put a more specific route before a less specific route.
  */
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       currentUser: null,
       isAdmin: false
     };
-    localStorage.setItem('currentUser', JSON.stringify(users[1]));
   }
 
   componentDidMount() {
-    AuthenticationService.currentUser.subscribe((x) =>
+    authenticationService.currentUser.subscribe((x) =>
       this.setState({
         currentUser: x,
         isAdmin: x && x.role === Role.Admin
@@ -54,25 +35,16 @@ class App extends React.Component {
   }
 
   render() {
+    // eslint-disable-next-line no-unused-vars
     const { currentUser, isAdmin } = this.state;
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <div>Logged out</div>
-            </Route>
-            {currentUser && (
-              <PrivateRoute
-                path="/dashboard"
-                roles={[currentUser.role]}
-                isAdmin={isAdmin}
-                component={Dashboard}
-              />
-            )}
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/dashboard/:userId" isAdmin={isAdmin} component={Dashboard} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/" exact component={LoginPage} />
+        </Switch>
+      </Router>
     );
   }
 }
