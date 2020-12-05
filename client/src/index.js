37d588ee7db050bb8a8939cabe0a064ea1f74cc0
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-05 21:35:14 +0100

    Add Popover functionality to Help

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.scss';
@@ -12,6 +11,8 @@ import App from './app';
 // ! Delete before production
 // setup fake backend
 import configureFakeBackend from './helpers/fake-backend';
+
+const $ = require('jquery');
 
 configureFakeBackend();
 
