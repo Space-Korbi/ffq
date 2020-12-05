a1dad97c9f6fb938a94ebabc64ab95892d126ecd
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-05 21:18:21 +0100

    Add Help component and styling

--- client/src/components/Question/Help.jsx
+++ client/src/components/Question/Help.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { string } from 'prop-types';
 import { QuestionIcon } from '@primer/octicons-react';
 
+import './styling/Help.css';
+
 const Help = ({ infoText }) => {
   return (
     <div className="text-info">
       <button
         type="button"
-        className="btn text-info"
+        className="btn text-info p-0"
         data-container="body"
         data-toggle="popover"
         data-placement="left"
         data-content={infoText}
       >
-        <QuestionIcon size="large" />
+        <QuestionIcon size="36" />
       </button>
     </div>
   );
