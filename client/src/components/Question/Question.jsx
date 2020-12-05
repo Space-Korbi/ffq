0e140b4068b179767e0231a5ab5b6ddac83378ec
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-05 13:33:48 +0100

    Set font size to 20px in Question popovers

--- client/src/components/Question/Question.jsx
+++ client/src/components/Question/Question.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Help from './Help';
 
-const mockInformation = 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus.';
+import './Question.css';
+
+const mockInformation =
+  'Vivamus sagittis lacus vel augue laoreet rutrum faucibus. is lacus vel augue laoreet rutrum faucibus.Vivamus sagittis lacus vel augue laoreet rutrum faucibus.Vivamus sagittis lacus vel augue laoreet rutrum faucibus.';
 
 const Question = () => {
   return (
