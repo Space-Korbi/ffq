38f42276929d372fce19d0be87ee7c29e3c265ca
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-08 03:07:56 +0100

    Move code from adminPanel component to AmountQuestion Fill AmountQuestion with mock Data

--- client/src/components/Question/AmountQuestion/AmountQuestion.jsx
+++ client/src/components/Question/AmountQuestion/AmountQuestion.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
-import { string } from 'prop-types';
+import { arrayOf, object, string } from 'prop-types';
 import Jumbotron from '../Jumbotron';
 import Help from '../Help';
+
+import pizzaWhole from '../../../images/pizza-whole-example.jpg';
+import pizzaHalf from '../../../images/pizza-half-example.jpg';
+import pizzaQuarter from '../../../images/pizza-quarter-example.jpg';
+
+const AmountCard = ({ image, title, subtitle }) => {
+  return (
+    <div className="card text-center" style={{ minWidth: '270px' }}>
+      <img src={image} className="card-img-top" alt="" />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+AmountCard.propTypes = {
+  image: string,
+  title: string.isRequired,
+  subtitle: string.isRequired
+};
+
+AmountCard.defaultProps = {
+  image: ''
+};
+
+const AmountCardsDeck = ({ amountCards }) => {
+  return amountCards.map((card) => <div className="px-2 align-self-center"> {card} </div>);
+};
+
+AmountCardsDeck.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  amountCards: arrayOf(object).isRequired
+};
+
+const mockAmountCards = [
+  <AmountCard key="1" title="1" subtitle="1.1" />,
+  <AmountCard key="2" image={pizzaQuarter} title="2" subtitle="2.1" />,
+  <AmountCard key="3" title="3" subtitle="3.1" />,
+  <AmountCard key="4" image={pizzaHalf} title="4" subtitle="4.1" />,
+  <AmountCard key="5" title="5" subtitle="5.1" />,
+  <AmountCard key="6" image={pizzaWhole} title="6" subtitle="6.1" />,
+  <AmountCard key="7" title="7" subtitle="7.1" />
+];
 
 const AmountQuestion = ({ title, subtitle, comment, help }) => {
   return (
@@ -12,6 +57,11 @@ const AmountQuestion = ({ title, subtitle, comment, help }) => {
       <div className="row no-gutters">
         <div className="col d-flex justify-content-end">
           <Help infoText={help} />
+        </div>
+        <div className="container-fluid px-0">
+          <div className="row no-gutters overflow-auto flex-row flex-nowrap text-center my-3">
+            <AmountCardsDeck amountCards={mockAmountCards} />
+          </div>
         </div>
       </div>
     </div>
