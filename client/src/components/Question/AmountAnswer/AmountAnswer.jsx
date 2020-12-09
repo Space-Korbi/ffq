9b2d0f4f020b4c7c93f295151c19b29784e9ff09
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-09 23:25:33 +0100

    Rename AmountCardsDeck

--- client/src/components/Question/AmountAnswer/AmountAnswer.jsx
+++ client/src/components/Question/AmountAnswer/AmountAnswer.jsx
@@ -1,55 +1,18 @@
 import React from 'react';
 import { arrayOf, string, shape } from 'prop-types';
 
-import AnswerCard from './AnswerCard';
+import AnswerCardsDeck from './AnswerCardsDeck';
 
-/**
- * * Amount Cards Spacing
- * @param amountCards - Array of card data
- * @returns [<Amount Card/>] - Amount cards wrapped in a div to apply extra spacing to the first and last card
- */
-const AmountCardsDeck = ({ amountCards }) => {
-  return amountCards.map((card, index) => {
-    switch (index) {
-      case 0:
-        return (
-          <div key={card.key} className="pl-5 pr-2 align-self-center">
-            <AnswerCard title={card.title} subtitle={card.subtitle} imageURL={card.imageURL} />
-          </div>
-        );
-      case amountCards.length - 1:
-        return (
-          <div key={card.key} className="pl-2 pr-5 align-self-center">
-            <AnswerCard title={card.title} subtitle={card.subtitle} imageURL={card.imageURL} />
-          </div>
-        );
-      default:
-        return (
-          <div key={card.key} className="px-2 align-self-center">
-            <AnswerCard title={card.title} subtitle={card.subtitle} imageURL={card.imageURL} />
-          </div>
-        );
-    }
-  });
+const saveAnswer = () => {
+  console.log('answer');
 };
 
-AmountCardsDeck.propTypes = {
-  amountCards: arrayOf(
-    shape({
-      key: string.isRequired,
-      title: string,
-      subtitle: string,
-      imageURL: string
-    })
-  ).isRequired
-};
-
-const AmountAnswer = ({ amountCards }) => {
+const AmountAnswer = ({ answerCards }) => {
   return (
     <div>
       <div className="container-fluid px-0">
         <div className="row no-gutters overflow-auto flex-row flex-nowrap text-center my-3">
-          <AmountCardsDeck amountCards={amountCards} />
+          <AnswerCardsDeck answerCards={answerCards} saveAnswer={saveAnswer} />
         </div>
       </div>
     </div>
@@ -57,7 +20,7 @@ const AmountAnswer = ({ amountCards }) => {
 };
 
 AmountAnswer.propTypes = {
-  amountCards: arrayOf(
+  answerCards: arrayOf(
     shape({
       key: string.isRequired,
       title: string.isRequired,
