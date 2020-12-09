9ed64838adc9bd8f00692f84254515dd6fe37bcc
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-09 19:34:17 +0100

    Fix Question Schema

--- server/models/question-model.js
+++ server/models/question-model.js
@@ -7,8 +7,8 @@ const { Schema } = mongoose;
  * @param uuid
  * @param index - Place in FFQ
  * @param type - One of the three main question types (Frequency Question, Amount Question or User Input Question)
- * @param parenQuestion -
- * @param childQuestion -
+ * @param parenQuestion - UUID of parent Question
+ * @param childQuestion - UUID of child Questions
  * @param isAnswered - Weather or not the question was answered
  * @param possibleAnswers - All Answers the user could have selected
  * @param answer - The answer the user has given
@@ -19,9 +19,12 @@ const Question = new Schema(
     questionUUID: { type: String, required: true, unique: true },
     index: { type: Number, required: true, unique: true },
     questionType: { type: String, required: true },
-    parenQuestion: { type: Schema.Types.ObjectId, required: false },
-    childQuestion: { type: Schema.Types.ObjectId, required: false },
-    title: { type: String, required: true },
+    parenQuestion: { type: String, required: false },
+    childQuestion: { type: [String], required: false },
+    title: { type: String },
+    subtitle1: { type: String },
+    subtitle2: { type: String },
+    help: { type: String },
     category: { type: String, required: true },
     possibleAnswers: { type: [String], required: true }
   },
