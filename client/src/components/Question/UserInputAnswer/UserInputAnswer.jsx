ceb5e3786ed63336aaac275ea0602fa72b4e3461
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-09 22:55:38 +0100

    Add UserInputAnswer component

---
+++ client/src/components/Question/UserInputAnswer/UserInputAnswer.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function UserInputAnswer(props) {
+  return <div>User Input Answer</div>;
+}
+
+UserInputAnswer.propTypes = {};
+
+export default UserInputAnswer;
--- client/src/components/Question/index.js
+++ client/src/components/Question/index.js
@@ -1,5 +1,6 @@
 import Question from './Question';
 import FrequencyAnswer from './FrequencyAnswer/FrequencyAnswer';
 import AmountAnswer from './AmountAnswer/AmountAnswer';
+import UserInputAnswer from './UserInputAnswer/UserInputAnswer';
 
-export { Question, FrequencyAnswer, AmountAnswer };
+export { Question, FrequencyAnswer, AmountAnswer, UserInputAnswer };
