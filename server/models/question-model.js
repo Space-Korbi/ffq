6ce2ba81a0355cb7d51d1aa55d2d37e4e5112ca2
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-10 02:44:07 +0100

    Change questionType to answerType in schema

--- server/models/question-model.js
+++ server/models/question-model.js
@@ -6,7 +6,7 @@ const { Schema } = mongoose;
  * * Question Schema
  * @param uuid
  * @param index - Place in FFQ
- * @param type - One of the three main question types (Frequency Question, Amount Question or User Input Question)
+ * @param answerType - One of the three main answer types (Frequency, Amount or User Input)
  * @param parenQuestion - UUID of parent Question
  * @param childQuestion - UUID of child Questions
  * @param isAnswered - Weather or not the question was answered
@@ -18,7 +18,7 @@ const Question = new Schema(
   {
     questionUUID: { type: String, required: true, unique: true },
     index: { type: Number, required: true, unique: true },
-    questionType: { type: String, required: true },
+    answerType: { type: String, required: true },
     parenQuestion: { type: String, required: false },
     childQuestion: { type: [String], required: false },
     title: { type: String },
