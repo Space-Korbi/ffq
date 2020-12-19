2cfb760ed05cbd109893721e5372ae0dfcbd7ec3
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-19 18:10:08 +0100

    Change Add Card button title

--- client/src/components/Editor/CardsEditor/CardsEditor.jsx
+++ client/src/components/Editor/CardsEditor/CardsEditor.jsx
@@ -31,7 +31,7 @@ const CardsEditor = ({ answerOptions, dispatch }) => {
             });
           }}
         >
-          Add New Amount Card
+          Add card
         </button>
       </div>
       <div>
