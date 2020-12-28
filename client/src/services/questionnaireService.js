dd97f9ec8adbb367886ee331ac0cd169dd30b81c
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-28 20:10:36 +0100

    Add questionnaire service middleware

---
+++ client/src/services/questionnaireService.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-alert */
+import { nanoid } from 'nanoid';
+import {
+  insertQuestion,
+  getAllQuestionsOfQuestionnaire,
+  insertQuestionnaire,
+  getAllQuestionnaires,
+  updateQuestionnaire,
+  deleteQuestionnaireById
+} from '../api';
+
+const updateAction = {
+  insert: 'insert',
+  insertAt: 'insertAt',
+  removeById: 'removeById',
+  moveUp: 'moveUp',
+  moveDown: 'moveDown'
+};
+
+const createQuestionnaire = async () => {
+  const payload = {
+    _id: nanoid()
+  };
+
+  return insertQuestionnaire(payload).then((res) => {
+    window.alert(`Questionnaire created successfully`);
+    return res.data;
+  });
+};
+
+const fetchAllQuestionnaires = async () => {
+  console.log('fetching questionnaires');
+
+  const questionnaires = await getAllQuestionnaires();
+  console.log('fetching questionnaires', questionnaires);
+  return questionnaires.data.data;
+};
+
+const createQuestionAt = async (questionnaireId, index) => {
+  const questionId = nanoid();
+
+  const questionPayload = {
+    _id: questionId
+  };
+
+  return insertQuestion(questionnaireId, questionPayload).then(async (question) => {
+    const questionnairePayload = {
+      action: updateAction.insert,
+      questionId: question.data.id
+    };
+    if (index) {
+      questionnairePayload.action = updateAction.insertAt;
+      questionnairePayload.index = index;
+    }
+    return updateQuestionnaire(questionnaireId, questionnairePayload).then((res) => {
+      window.alert(`Question added successfully at ${index}`);
+      return res.data;
+    });
+  });
+};
+
+const removeQuestionById = (questionnaireId, questionId) => {
+  const questionnairePayload = {
+    action: updateAction.removeById,
+    questionId
+  };
+  return updateQuestionnaire(questionnaireId, questionnairePayload).then((res) => {
+    return res.data;
+  });
+};
+
+// const moveQuestionTo = async (questionnaireId, questionId, index) => {};
+
+const fetchAllQuestionsOfQuestionnaire = async (questionnaireId) => {
+  if (!questionnaireId) {
+    return [];
+  }
+
+  const questions = await getAllQuestionsOfQuestionnaire(questionnaireId);
+  return questions.data.data;
+};
+
+const deleteQuestionnaire = async (id) => {
+  const deletedQuestionnaire = await deleteQuestionnaireById(id);
+  return deletedQuestionnaire.data.data;
+};
+const questionnaireService = {
+  createQuestionnaire,
+  createQuestionAt,
+  fetchAllQuestionnaires,
+  fetchAllQuestionsOfQuestionnaire,
+  removeQuestionById,
+  deleteQuestionnaire
+};
+
+export default questionnaireService;
