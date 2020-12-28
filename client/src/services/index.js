5b21ad23ca788237444fcb9083d5a04b57e735b0
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-28 20:13:45 +0100

    Export questionnaireService from index.js

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,5 +1,6 @@
 import authenticationService from './authenticationService';
 import questionService from './questionService';
+import questionnaireService from './questionnaireService';
 import ffqService from './ffqService';
 
-export { authenticationService, questionService, ffqService };
+export { authenticationService, questionService, questionnaireService, ffqService };
