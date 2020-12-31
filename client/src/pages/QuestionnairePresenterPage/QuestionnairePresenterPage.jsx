09233e51bd3073cc56c28072f5726cd53e9ecdd7
Korbinian Baumer <baumer.korbinian@gmail.com>
2020-12-31 19:03:23 +0100

    Add basic navigation to new questionnaire presenter

--- client/src/pages/QuestionnairePresenterPage/QuestionnairePresenterPage.jsx
+++ client/src/pages/QuestionnairePresenterPage/QuestionnairePresenterPage.jsx
@@ -1,18 +1,45 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useState } from 'react';
 import { string } from 'prop-types';
 
 // custom hooks
-import useFetchQuestions from '../../hooks';
+import { useFetchQuestions } from '../../hooks';
+
+// components
+import { Question } from '../../components/Question';
 
 const QuestionnairePresenterPage = ({ questionnaireId }) => {
   const [{ questions, isLoading, isError }] = useFetchQuestions(questionnaireId);
-  console.log(questions);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <div>
       {isError && <div>Something went wrong ...</div>}
-      {isLoading ? 'Loading...' : 'Welcome to QuestionnairePresenterPage'}
+      {isLoading ? (
+        'Loading...'
+      ) : (
+        <div>
+          {questions.length && (
+            <div>
+              <Question
+                id={questions[currentIndex]._id}
+                title={questions[currentIndex].title}
+                subtitle1={questions[currentIndex].subtitle1}
+                subtitle2={questions[currentIndex].subtitle2}
+                help={questions[currentIndex].help}
+                answerOptions={questions[currentIndex].answerOptions}
+                answerType={questions[currentIndex].answerType}
+              />
+              <button type="button" onClick={() => setCurrentIndex(currentIndex - 1)}>
+                Back
+              </button>
+              <button type="button" onClick={() => setCurrentIndex(currentIndex + 1)}>
+                Forward
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
