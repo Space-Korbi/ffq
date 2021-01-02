a5072107798a07e237a9007cf6e99c3d22d6a5d7
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-01-02 01:32:05 +0100

    Moving db initialisation code into models folder

--- server/index.js
+++ server/index.js
@@ -23,11 +23,51 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // serve static files such as images
 app.use(express.static('uploads'));
 
-/**
- * * Database
- * ./db is required to establish a DB connection
- */
-require('./db');
+// * Database
+const db = require('./models');
+const dbConfig = require('./config/db.config');
+
+const Role = db.role;
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: 'user'
+      }).save((err) => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: 'admin'
+      }).save((err) => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Successfully connect to MongoDB.');
+    initial();
+  })
+  .catch((err) => {
+    console.error('Connection error', err);
+    process.exit();
+  });
 
 // Routes
 const questionnaireRouter = require('./routes/questionnaire-router');
