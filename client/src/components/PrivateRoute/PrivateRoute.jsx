659ab72690c908977f1e7e28279c2e7f5d00439f
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-01-03 01:54:23 +0100

    Adjust PrivateRoute to work with updated AuthService

--- client/src/components/PrivateRoute/PrivateRoute.jsx
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes, { shape, string } from 'prop-types';
-import { authenticationService } from '../../services';
+import authService from '../../services/auth.service';
+// import { authenticationService } from '../../services';
 
 /**
  * * PrivateRoute
@@ -14,8 +15,8 @@ const PrivateRoute = ({ location, roles, isAdmin, component: Component, ...rest 
     <Route
       {...rest}
       render={() => {
-        const currentUser = authenticationService.currentUserValue;
-        if (!currentUser) {
+        const { currentUserValue } = authService;
+        if (!currentUserValue) {
           return (
             <Redirect
               to={{
@@ -27,9 +28,9 @@ const PrivateRoute = ({ location, roles, isAdmin, component: Component, ...rest 
         }
 
         // check if route is restricted by role
-        if (roles && roles.indexOf(currentUser.role) === -1) {
+        if (roles && roles.indexOf(currentUserValue.roles[0]) === -1) {
           // role not authorised so redirect to home page
-          return <Redirect to={{ pathname: `/dashboard/${currentUser.id}` }} />;
+          return <Redirect to={{ pathname: `/dashboard/${currentUserValue.id}` }} />;
         }
 
         // authorised so return component
