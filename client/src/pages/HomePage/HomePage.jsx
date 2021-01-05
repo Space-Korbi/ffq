2c4cee00ddb3f155a333b334a2667a453ef6f42c
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-01-06 00:50:25 +0100

    Add styling to home page and add custom header message

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -2,25 +2,30 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 
-const HomePage = ({ stoppedAtIndex }) => {
+const HomePage = ({ isAdmin, stoppedAtIndex }) => {
   const { url } = useRouteMatch();
+  console.log(isAdmin);
 
-  let title = `Um die Umfrage zu starten klicken Sie bitte auf 'Umfrage starten'`;
+  let header = 'Willkommen!';
+  let title = `Um die Umfrage zu starten klicken Sie bitte auf "Umfrage starten"`;
   let button = `Umfrage starten`;
 
-  if (stoppedAtIndex) {
-    title = `Um die Umfrage fortzusetzen klicken Sie bitte auf 'Umfrage fortsetzen'`;
+  if (stoppedAtIndex > -1 && !isAdmin) {
+    header = 'Willkommen zurück!';
+    title = `Um die Umfrage fortzusetzen klicken Sie bitte auf "Umfrage fortsetzen"`;
     button = `Umfrage fortsetzen`;
   }
   return (
     <div className="d-flex justify-content-center mt-3">
-      <div className="jumbotron" style={{ maxWidth: '800px' }}>
-        <h1 className="display-4">Hallo</h1>
+      <div className="jumbotron text-center" style={{ maxWidth: '800px' }}>
+        <h1 className="display-4">{header}</h1>
         <p className="lead">{title}</p>
         <hr className="my-4" />
-        <Link className="btn btn-primary btn-large" to={`${url}/questionnairePresenter`}>
-          {button}
-        </Link>
+        <div className="text-center">
+          <Link className="btn btn-primary btn-large" to={`${url}/questionnairePresenter`}>
+            {button}
+          </Link>
+        </div>
       </div>
     </div>
   );
