bc6d2d4876bddab9b940d8d0652a663e71680d29
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-01-06 00:19:24 +0100

    Add basic AccountPage layout

--- client/src/pages/AccountPage/AccountPage.jsx
+++ client/src/pages/AccountPage/AccountPage.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const AccountPage = () => {
-  return <div>Account info and form to change stuff</div>;
+// eslint-disable-next-line react/prop-types
+const AccountPage = ({ firstName, lastName, email, hasAcceptedConsentForm }) => {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <header className="mb-5">
+          <h3>
+            <strong>{`${firstName} ${lastName}`}</strong>
+          </h3>
+        </header>
+        <p>
+          <strong>Email:</strong> {email}
+        </p>
+        <p>
+          <strong>Consent Form:</strong>{' '}
+          {hasAcceptedConsentForm ? (
+            <span className="badge badge-success">Accepted</span>
+          ) : (
+            <span className="badge badge-danger">Not yet accepted</span>
+          )}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AccountPage;
