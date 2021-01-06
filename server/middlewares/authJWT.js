011b329a316b810c4226608454b9211083f22a4c
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-01-06 01:36:28 +0100

    Fix issue with verifying if user is Admin

--- server/middlewares/authJWT.js
+++ server/middlewares/authJWT.js
@@ -37,7 +37,7 @@ const isAdmin = (req, res, next) => {
           return;
         }
 
-        for (let i = 0; i < roles.length; i++) {
+        for (let i = 0; i < roles.length; i += i) {
           if (roles[i].name === 'admin') {
             next();
             return;
