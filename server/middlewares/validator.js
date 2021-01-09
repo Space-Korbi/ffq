657c225f29476d3fe5b32af63b9f2de08d1b9b04
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-01-09 12:01:47 +0100

    Add validator.js file Validator validates user input in request bodies Validate signup and login request bodies

---
+++ server/middlewares/validator.js
@@ -0,0 +1,62 @@
+const { body, validationResult } = require('express-validator');
+
+// Maybe body should be escaped
+
+const signup = [
+  body('firstName').notEmpty().withMessage('First name cannot be empty.'),
+  body('lastName').notEmpty().withMessage('Last name cannot be empty.'),
+  body('email')
+    .notEmpty()
+    .withMessage('Email cannot be empty.')
+    .bail()
+    .isEmail()
+    .withMessage('Please enter a valid email address.'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password cannot be empty.')
+    .isLength({ min: 5 })
+    .withMessage('Password must be at least 5 characters long.')
+];
+
+const login = [
+  body('email')
+    .notEmpty()
+    .withMessage('Email cannot be empty.')
+    .bail()
+    .isEmail()
+    .withMessage('Please enter a valid email address.'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password cannot be empty.')
+    .isLength({ min: 5 })
+    .withMessage('Password must be at least 5 characters long.')
+];
+
+const userValidationRules = () => {
+  return [
+    // username must be an email
+    body('email').isEmail(),
+    // password must be at least 5 chars long
+    body('password').isLength({ min: 6 })
+  ];
+};
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
+
+  return res.status(422).json({
+    errors: extractedErrors
+  });
+};
+
+module.exports = {
+  login,
+  signup,
+  userValidationRules,
+  validate
+};
--- server/users/user.router.js
+++ server/users/user.router.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const UserCtrl = require('./user.controller');
-const { authJwt } = require('../middlewares');
+const { authJwt, validate } = require('../middlewares');
 
 const router = express.Router();
+
+// signup
+router.post('/users/signup', validate.signup, UserCtrl.createUser);
+
+// login
+router.post('/users/login', validate.login, UserCtrl.loginUser);
 
 router.get('/users', [authJwt.verifyToken, authJwt.isAdmin], UserCtrl.getUsers);
 router.get('/users/:userId', [authJwt.verifyToken, authJwt.authoriseUser], UserCtrl.getUserById);
