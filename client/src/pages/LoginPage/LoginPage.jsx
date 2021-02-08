20bc43bf7416b633d5cc502d979d310d3fe53677
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-02-08 04:36:31 +0100

    Remove placeholder

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -38,8 +38,8 @@ const LoginPage = () => {
           <br />
           <Formik
             initialValues={{
-              email: 'u@1.de',
-              password: '12345'
+              email: '',
+              password: ''
             }}
             validationSchema={Yup.object().shape({
               email: Yup.string().required('Email is required'),
