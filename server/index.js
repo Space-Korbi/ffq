aa2cfcf73fd4c26b939e89895af09a9cf0c96589
Korbinian Baumer <baumer.korbinian@gmail.com>
2021-05-07 14:21:22 +0200

    Setting host to 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -103,4 +103,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, process.env.HOST, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
